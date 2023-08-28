64775c90e13ee2649c733959f473fb0aa535387e
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-08-28 21:01:21 +0700

    Write a rough pseudo code for the project

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 document.write("This is a test");
+alert("Hey Listen!");
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+console.log(sum(5,2));
+
+// Write function to get computer random input ranging from rock, paper, scissor.
+// write function get user input ranging from rock, paper, scissor.
+// write function that "play" a single round of rock paper scissor and declare the winner
+// write function that construct a game of rock paper scissor where you play 5 round and keep score and report the winner at the end
+// write function to restart a round
