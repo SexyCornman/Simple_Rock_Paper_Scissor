9d45ad101ad79085ee4c502ec571265ac83ae877
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-08-28 22:46:16 +0700

    add ;

--- script.js
+++ script.js
@@ -33,25 +33,25 @@ function playerChoiceScissor() {
 function gameStart() {
     getComputerChoice()
     if (playerChoice === "Rock" && computerChoice === "Paper") {
-        alert("You Suck paper beat Rock!")
+        alert("You Suck paper beat Rock!");
     } else if (playerChoice === "Rock" && computerChoice === "Rock") {
-        alert("It's a tie you both suck!")
+        alert("It's a tie you both suck!");
     } else if (playerChoice === "Rock" && computerChoice === "Scissor") {
-        alert ("You win! fucking hell")
+        alert ("You win! fucking hell");
     } 
     else if (playerChoice === "Paper" && computerChoice === "Paper") {
         alert("It's a tie you both suck!")
     } else if (playerChoice === "Paper" && computerChoice === "Rock") {
-        alert("You win! fucking hell")
+        alert("You win! fucking hell");
     } else if (playerChoice === "Paper" && computerChoice === "Scissor") {
         alert ("You Suck scissor beat paper!")
     }
     else if (playerChoice === "Scissor" && computerChoice === "Paper") {
-        alert("You win! fucking hell!")
+        alert("You win! fucking hell!");
     } else if (playerChoice === "Scissor" && computerChoice === "Rock") {
-        alert("You Suck rock beat scissor! ")
+        alert("You Suck rock beat scissor! ");
     } else if (playerChoice === "Scissor" && computerChoice === "Scissor") {
-        alert ("It's a tie you both suck!!")
+        alert ("It's a tie you both suck!!");
     }
 }
 
