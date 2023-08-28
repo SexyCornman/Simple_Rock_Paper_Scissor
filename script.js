c29833f516b050e7ead0b0de89d28fa22430c480
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-08-28 22:40:27 +0700

    add playerChoice and gameStart function
    instead of let each button run an individual game of rock paper scissor separately.
    
    I wrote  new functions so every time player pick a choice their choice will be store in the variable and will be calculated in the "game" code

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let gameChoice = ["Rock","Paper","Scissor"]
 let computerChoice
 function getComputerChoice() {
     computerChoice = gameChoice[(Math.floor(Math.random() * gameChoice.length))];   
-    console.log(computerChoice);
+    alert("The computer choose " + computerChoice);
 }
 
 
@@ -13,41 +13,48 @@ function getComputerChoice() {
 
 
 // write function get user input ranging from rock, paper, scissor.
-// write function that "play" a single round of rock paper scissor and declare the winner
+let playerChoice
 function playerChoiceRock() {
-    getComputerChoice()
-    if (computerChoice === "Paper") {
-        alert("You Suck paper beat Rock!")
-    } else if (computerChoice === "Rock") {
-        alert("It's a tie you both suck!")
-    } else if (computerChoice === "Scissor") {
-        alert ("You win! fucking hell")
-    }
-   
-
+    playerChoice = "Rock";
+    gameStart();
 }
+
 function playerChoicePaper() {
-    getComputerChoice()
-    if (computerChoice === "Paper") {
-        alert("It's a tie you both suck!")
-    } else if (computerChoice === "Rock") {
-        alert("You win! fucking hell")
-    } else if (computerChoice === "Scissor") {
-        alert ("You Suck scissor beat paper!")
-    }
-    
+    playerChoice = "Paper";
+    gameStart();
 }
 
 function playerChoiceScissor() {
+    playerChoice = "Scissor";
+    gameStart();
+}
+
+// write function that "Start" a single round of rock paper scissor and declare the winner
+function gameStart() {
     getComputerChoice()
-    if (computerChoice === "Paper") {
-        alert("You win! fucking hel!")
-    } else if (computerChoice === "Rock") {
-        alert("You Suck rock beat scissor! ")
-    } else if (computerChoice === "Scissor") {
+    if (playerChoice === "Rock" && computerChoice === "Paper") {
+        alert("You Suck paper beat Rock!")
+    } else if (playerChoice === "Rock" && computerChoice === "Rock") {
+        alert("It's a tie you both suck!")
+    } else if (playerChoice === "Rock" && computerChoice === "Scissor") {
+        alert ("You win! fucking hell")
+    } 
+    else if (playerChoice === "Paper" && computerChoice === "Paper") {
+        alert("It's a tie you both suck!")
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        alert("You win! fucking hell")
+    } else if (playerChoice === "Paper" && computerChoice === "Scissor") {
         alert ("You Suck scissor beat paper!")
     }
-    
+    else if (playerChoice === "Scissor" && computerChoice === "Paper") {
+        alert("You win! fucking hell!")
+    } else if (playerChoice === "Scissor" && computerChoice === "Rock") {
+        alert("You Suck rock beat scissor! ")
+    } else if (playerChoice === "Scissor" && computerChoice === "Scissor") {
+        alert ("It's a tie you both suck!!")
+    }
 }
+
+
 // write function that construct a game of rock paper scissor where you play 5 round and keep score and report the winner at the end
 // write function to restart a round
